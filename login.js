fccd766dc50573a1e585141d906ea50a1ee88bb7
bharathautomation <139088966+bharathtechacademy@users.noreply.github.com>
2026-08-17 10:00:45 +0530

    Revert "new change 2"
    This reverts commit 0c75c4f7d1036b85a3c9d11d2c73e523b81f4d34.

--- login.js
+++ login.js
@@ -5,7 +5,3 @@ console.log("Login Page - Line 4 Added By Bharath");
 console.log("Login Page - Line 5 Added By Bharath");
 console.log("Login Page - Line 4 Added By Fagun");
 console.log("Login Page - Line 5 Added By Fagun");
-console.log("Login Page - Line 6 Added By Fagun");
-console.log("Login Page - Line 7 Added By Fagun");
-console.log("Login Page - Line 8 Added By Bharath");
-
